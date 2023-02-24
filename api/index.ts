f5079eeb4059ec2aa70695d64bdf524ae4ead57b
kapysta000017 <kapysta000017@gmail.com>
2023-02-24 21:10:01 +0300

    add dirs api and client

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,8 @@
+export {}
 const jsonServer = require("json-server")
 const server = jsonServer.create()
-const router = jsonServer.router("./public/db.json")
-const middlewares = jsonServer.defaults({
-  static: "./build",
-})
+const router = jsonServer.router("./db.json")
+const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 3001
 
